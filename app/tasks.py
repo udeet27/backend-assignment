923fd51a2ee4c41bfbed53e226f6e1f1c18603b5
Udeet Mittal <udeet1575@gmail.com>
2025-01-29 18:05:48 +0530

    backend assignment push

--- app/tasks.py
+++ app/tasks.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import Session
 from .database import SessionLocal
 from . import models
 import openai
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
 
 celery_app = Celery('tasks', broker='redis://default:bS7wTdkZR7XtTrPfqtF6RO7cPsjeqZL6@redis-15809.c305.ap-south-1-1.ec2.redns.redis-cloud.com:15809')
 
@@ -23,7 +28,9 @@ def analyze_review_sentiment(review_id: int):
         review = db.query(models.ReviewHistory).filter(models.ReviewHistory.id == review_id).first()
         if review and (review.tone is None or review.sentiment is None):
             # Use OpenAI API to analyze sentiment and tone
-            openai.api_key = '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'
+
+            # Access the API key
+            openai.api_key = os.getenv('API_KEY')
             response = openai.Completion.create(
                 engine="text-davinci-002",  
                 prompt=f"Analyze the tone and sentiment of this review:\n\nText: {review.text}\nStars: {review.stars}\n\nTone:",
